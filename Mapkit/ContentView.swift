9940cdf974d145f5cdd945eb5416030c89683e18
Shohei <yukawa.shouhei@gmail.com>
2025-05-30 19:59:12 +0900

    Initial Commit

---
+++ Mapkit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mapkit
+//
+//  Created by 湯川昇平 on 2025/05/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mapkit/MapkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapkitApp.swift
+//  Mapkit
+//
+//  Created by 湯川昇平 on 2025/05/30.
+//
+
+import SwiftUI
+
+@main
+struct MapkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
