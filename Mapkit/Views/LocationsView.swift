7f1216565a6d55a7217ba899b0819edff9c9541e
Shohei <yukawa.shouhei@gmail.com>
2025-06-07 17:17:48 +0900

    メニューの実装

--- Mapkit/Views/LocationsView.swift
+++ Mapkit/Views/LocationsView.swift
@@ -17,6 +17,14 @@ struct LocationsView: View {
         ZStack {
             Map(coordinateRegion: $vm.mapRegion)
                 .ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+                header
+                    .padding()
+                
+                Spacer()
+                
+            }
         }
     }
 }
@@ -24,4 +32,31 @@ struct LocationsView: View {
 #Preview {
     LocationsView()
         .environmentObject(LocationsViewModel())
+}
+
+extension LocationsView {
+    
+    private var header: some View {
+        VStack {
+            Text(vm.mapLocation.name + "," + vm.mapLocation.cityName)
+                .font(.title2)
+                .fontWeight(.black)
+                .foregroundColor(.primary)
+                .frame(height:55)
+                .frame(maxWidth: .infinity)
+                .overlay(alignment: .leading) {
+                    Image(systemName: "arrow.down")
+                        .font(.headline)
+                        .foregroundColor(.primary)
+                        .padding()
+                }
+            
+            
+        }
+        .background(.thickMaterial)
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
+        
+    }
+    
 }
