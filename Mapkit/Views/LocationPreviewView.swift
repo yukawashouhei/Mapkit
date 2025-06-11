0322ee46b18a0c49ae69d3f1435a2a9963e73c2b
Shohei <yukawa.shouhei@gmail.com>
2025-06-12 07:39:02 +0900

    詳細ページの実装

---
+++ Mapkit/Views/LocationPreviewView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationPreviewView.swift
+//  Mapkit
+//
+//  Created by 湯川昇平 on 2025/06/09.
+//
+
+import SwiftUI
+
+struct LocationPreviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationPreviewView()
+}
